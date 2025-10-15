7b5777dd96a486d01434bfa54489c67f149627a2
Tumi <Apple@MacBook-Pro.local>
2025-10-15 13:21:32 +0100

    Week 3 Practice 1 - Basic Rust variables and printing

---
+++ target/debug/deps/practice_1-ca86158ad5c8e349.d
@@ -0,0 +1,5 @@
+/Users/Apple/Documents/T.odusolaCOS101/week-3/practice_1/target/debug/deps/practice_1-ca86158ad5c8e349.d: src/main.rs
+
+/Users/Apple/Documents/T.odusolaCOS101/week-3/practice_1/target/debug/deps/practice_1-ca86158ad5c8e349: src/main.rs
+
+src/main.rs:
