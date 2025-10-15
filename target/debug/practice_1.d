ab791341bb467bcfaba98062b21bd89bccab3f8f
Tumi <Apple@MacBook-Pro.local>
2025-10-15 20:55:31 +0100

    added practice_9

--- target/debug/practice_1.d
+++ target/debug/practice_1.d
@@ -1,1 +1,1 @@
-/Users/Apple/Documents/T.odusolaCOS101/week-3/practice_1/target/debug/practice_1: /Users/Apple/Documents/T.odusolaCOS101/week-3/practice_1/src/main.rs
+/Users/Apple/Documents/T.odusolaCOS101/target/debug/practice_1: /Users/Apple/Documents/T.odusolaCOS101/src/main.rs
--- week-3/practice_9/src/main.rs
+++ week-3/practice_9/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let mut fees: i32 = 25_000; // declare mutable integer variable
+    println!("fees is {}", fees);
+
+    fees = 35_000; // update the value
+    println!("fees changed is {}", fees);
 }
