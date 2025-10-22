8d7a7685416f896f0548095c9e0e595bb5f25987
Tumi <Apple@MacBook-Pro.local>
2025-10-22 13:27:12 +0100

    This Rust program asks for the user’s name and age, then checks if they are 18 or older to decide if they can enter the party.

--- week-4/practice_4/src/main.rs
+++ week-4/practice_4/src/main.rs
@@ -1,3 +1,22 @@
+// Rust program to determine age pass
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+
+    println!("Enter your name: ");
+    io::stdin().read_line(&mut input1).expect("Not a valid string");
+
+    println!("Enter your age: ");
+    io::stdin().read_line(&mut input2).expect("Not a valid string");
+
+    let age: i32 = input2.trim().parse().expect("Not a valid number");
+
+    if age >= 18 {
+        println!("Welcome to the party, {}!", input1.trim());
+    } else {
+        println!("Oops, you are not of age to enter the party, {}!", input1.trim());
+    }
 }
