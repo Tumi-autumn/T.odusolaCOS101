a2a96948a43c8ece9ec54105ed884cd18c9d0fa8
Tumi <Apple@MacBook-Pro.local>
2025-11-12 14:06:35 +0100

    practice_5

--- week-7/practice_5/src/main.rs
+++ week-7/practice_5/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let num: i32 = 5;
+    // When passed to the function, the value of 'num_to_zero' is copied.
+    let num_to_zero = num; 
+    
+    // Call the function
+    mutate_num_to_zero(num_to_zero);
+
+    // The original variables remain 5
+    println!("The value of num is: {}", num);
+    println!("The value of num_to_zero is: {}", num_to_zero); 
+}
+
+// Function receives a copy of the value
+fn mutate_num_to_zero(mut param_num: i32) {
+    param_num = 0; // Only the local copy is changed
+    println!("param_num value is: {}", param_num); // Prints 0
 }
