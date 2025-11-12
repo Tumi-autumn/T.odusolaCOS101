604d75fa557d5007a6e092fd104d7a29a81a9d21
Tumi <Apple@MacBook-Pro.local>
2025-11-12 14:03:27 +0100

    array with for loop

--- week-7/practice_9/src/main.rs
+++ week-7/practice_9/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let arr: [i32; 5] = [10, 20, 30, 40, 50]; // an array stores functions of all elements of same data type
+    println!("Array is: {:?}", arr);
+    let mut sum: i32 = 0;
+    for val in arr.iter() {
+        sum += val;
+    }
+    println!("Sum is: {}", sum);
 }
