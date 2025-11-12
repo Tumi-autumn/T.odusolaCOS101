17b74f6665b8935d1a97c32efeb90e4fc95fc920
Tumi <Apple@MacBook-Pro.local>
2025-11-12 14:02:21 +0100

    array with loop

--- week-7/practice_8/src/main.rs
+++ week-7/practice_8/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let city_arr: [&str; 5] = ["Abuja", "PortHarcourt", "Maiduguri", "Kano", "Lagos"];
+    let mut index: usize = 0;
+    while index < city_arr.len() {
+        println!("Array size is {}", city_arr.len());
+        index += 1;
+    }
+    for index in 0..city_arr.len() {
+        println!("City index {} is located in {}", index, city_arr[index]);
+    }
 }
