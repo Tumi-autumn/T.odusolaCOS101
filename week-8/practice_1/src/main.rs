25bf625b040646fc4b78078ad3ec1bae24b565a4
Tumi <Apple@MacBook-Pro.local>
2025-11-19 12:58:59 +0100

    creating a vector in rust

---
+++ week-8/practice_1/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    let v : Vec<i64> = Vec::new();
+    println!("\nThe lenght of Vec::new is: {}", v.len());
+    let v = vec!["Grace", "Effiong", "Basil", "Kareem", "susan"];
+    println!("\nThe lenght of vec macro is: {}", v.len());
+
+}
